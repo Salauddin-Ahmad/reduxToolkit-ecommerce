6b49c2aaeb5be55563ea3f913b24aead13f2d086
Md. Salauddin <salauddinahmed555@gmail.com>
2024-09-04 22:16:43 +0600

    created reducers and fetched data with  asyncthunk

--- src/features/products/ProductListView.jsx
+++ src/features/products/ProductListView.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { fetchProducts } from "./productSlice";
+import { useDispatch } from "react-redux";
 const ProductListView = () => {
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        dispatch(   fetchProducts())
+    }, [])
+
     return (
         <div>
             ajkfsdhlsajk
--- src/features/products/productSlice.js
+++ src/features/products/productSlice.js
@@ -1,16 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     products:[],
     isLoading: false,
     error: null,
 };
+const BASE_URL = 'http://localhost:3003/products'
+
+export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
+        const res = await axios.get(BASE_URL);
+        return res.data;
+})
 
 export const productSlice = createSlice({
     name: 'products',
     initialState: initialState,
     reducers: {
       
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchProducts.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        })
+        .addCase(fetchProducts.fulfilled, (state, action) => {
+            state.products = action.payload;
+            state.isLoading = false;
+        })
+        .addCase(fetchProducts.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = "Failed to fetch data" || action.error.message;
+        })
     }
 })
 
